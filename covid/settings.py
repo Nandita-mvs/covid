16be7edf450bec95e50ca42122a2cda380c4c976
Nandita-mvs <67993588+Nandita-mvs@users.noreply.github.com>
2021-05-03 22:19:28 +0530

    Update settings.py

--- covid/settings.py
+++ covid/settings.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
-"""
 
-from whitenoise.django import DjangoWhiteNoise
 import django_heroku
 from pathlib import Path
 import os
